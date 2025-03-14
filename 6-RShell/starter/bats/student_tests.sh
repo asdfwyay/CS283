a3d414969f6008f8996c0966c73b3f85e86d1166
asdfwyay <0asdfwyay0@gmail.com>
2025-03-14 01:41:51 -0400

    add student tests

--- 6-RShell/starter/bats/student_tests.sh
+++ 6-RShell/starter/bats/student_tests.sh
@@ -2,21 +2,157 @@
 
 # File: student_tests.sh
 # 
-# Create your unit tests suit in this file
+# Create your unit tests suite in this file
+
+## NOTE: During bats testing, all tests will print the server startup message upon calling built-in commands (exit, rc, etc.).
+## This does not happen outside of bats testing.
 
 setup() {
-    ./dsh -s
+    sleep 0.5
+    ./dsh -s > /dev/null 2>&1 &
+    sleep 0.5
 }
 
-@test "Connect to server" {
-    run ./dsh -c <<EOF                
+@test "Connect to server (default host & port)" {
+    run ./dsh -c <<EOF  
 EOF
 
     # Strip all whitespace (spaces, tabs, newlines) from the output
     stripped_output=$(echo "$output" | tr -d '[:space:]')
 
     # Expected output with all whitespace removed for easier matching
-    expected_output="dshlib.clocalmodedsh4>dsh4>cmdloopreturned0"
+    expected_output="socketclientmode:addr:127.0.0.1:5678dsh4>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Remote pwd" {
+    run ./dsh -c <<EOF
+pwd
+EOF
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Obtain current working directory
+    wd=$(pwd)
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="socketclientmode:addr:127.0.0.1:5678dsh4>${wd}dsh4>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Remote pipes" {
+    run ./dsh -c <<EOF
+ls | grep dshlib.c
+EOF
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="socketclientmode:addr:127.0.0.1:5678dsh4>dshlib.cdsh4>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Run multiple + built-in commands" {
+    run ./dsh -c <<EOF
+false
+rc
+EOF
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="socketclientmode:addr:127.0.0.1:5678dsh4>dsh4>socketservermode:addr:0.0.0.0:5678->Single-ThreadedMode1dsh4>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Disconnect client from server (exit)" {
+    run ./dsh -c <<EOF
+exit
+EOF
+    jobs
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="socketclientmode:addr:127.0.0.1:5678dsh4>socketservermode:addr:0.0.0.0:5678->Single-ThreadedModeBye!cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Stop server" {
+    run ./dsh -c <<EOF
+stop-server
+EOF
+    jobs
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="socketclientmode:addr:127.0.0.1:5678dsh4>socketservermode:addr:0.0.0.0:5678->Single-ThreadedModeBye!dsh4>cmdloopreturned0"
 
     # These echo commands will help with debugging and will only print
     #if the test fails
